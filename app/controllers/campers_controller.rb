9c6fa9c6b7e4c72c0fa40396f8cc36fb4a3a9459
Hunter Wood <hunter674530@gmail.com>
2023-03-01 14:19:44 -0600

    commit tree 1707925898fcffa104c9be8eebca73b7d1b0b564

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -7,7 +7,7 @@ class CampersController < ApplicationController
 
   def show
     camper = Camper.find(params[:id])
-    render json: camper, status: :ok
+    render json: camper, include: :activities, status: :ok
   end
 
   def create
